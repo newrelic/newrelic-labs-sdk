23b66126b0dfe850caf67180e31b4688c4058ca8
Scott DeWitt <sdewitt@newrelic.com>
2024-07-12 10:54:55 -0400

    fix: add lambda integration to avoid flag redefined error

---
+++ pkg/integration/lambda.go
@@ -0,0 +1,48 @@
+package integration
+
+import (
+	"github.com/newrelic/newrelic-labs-sdk/pkg/integration/log"
+	"github.com/spf13/viper"
+)
+
+func NewLambdaIntegration(
+	buildInfo *BuildInfo,
+	appName string,
+	labsIntegrationOpts ...LabsIntegrationOpt,
+) (*LabsIntegration, error) {
+	// We don't setup/process command line flags since we are running as a
+	// lambda.
+
+	// Load configuration with viper
+	// We have to do this prior to setting up the APM app because the license
+	// key may be in the config.
+	err := loadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	// Now that the config is loaded, setup logging
+	err = setupLogging(log.RootLogger)
+	if err != nil {
+		return nil, err
+	}
+
+	// Setup APM
+	app, err := setupApm(appName, log.RootLogger)
+	if err != nil {
+		return nil, err
+	}
+
+	defer log.Debugf("starting %s integration", buildInfo.Name)
+
+	// Create the integration
+	return newLabsIntegration(
+		buildInfo,
+		app,
+		nil,		// this is not an infra integration
+		log.RootLogger,
+		false,		// can't run as service when running as a lambda
+		viper.GetBool("dry_run"),
+		labsIntegrationOpts,
+	)
+}
